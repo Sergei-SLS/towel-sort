8b6c9c9950684def79d3117b14c94ce053e48a7a
Sergei-SLS <lyamin.sls@gmail.com>
2023-02-13 21:34:06 +0600

    docs: create matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return [];
+  }
+  let arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let column = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+      arr.push(matrix[i][column]);
+    }
+  }
+  return arr;
 }
+
